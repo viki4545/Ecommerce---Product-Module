cdb94d759d5e631a4a2e0ffd6df6065c44affeab
Vicky <vickygupta9987@gmail.com>
2025-02-11 21:48:35 +0530

    changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
